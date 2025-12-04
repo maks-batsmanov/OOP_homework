0a7a17a458f5e98d1d26fdd99d26d66230e65b7c
Максим Бацманов <markgusev995@gmail.com>
2025-12-04 14:34:20 +0300

    fixed tests/test_class_category.py

--- tests/test_class_category.py
+++ tests/test_class_category.py
@@ -17,7 +17,7 @@ def test_count_product(category_books):
     Category.product_count = 0
 
     Category.category_count += 1
-    Category.product_count += len(category_books.products)
+    Category.product_count += len(category_books._Category__products)
 
     assert Category.product_count == 3
     assert Category.category_count == 1
@@ -27,8 +27,8 @@ def test_add_product(category_books):
     new_product = Product("Twenty thousand leagues under the sea", "fantasy", 300.00, 2)
     category_books.add_product(new_product)
     assert category_books.products == (
-        "Harry Potter, 100. руб. Остаток: 10 шт."
-        "Lord of the Rings, 120. руб. Остаток: 9 шт."
-        "The Chronicles of Narnia, 130. руб. Остаток: 12 шт."
-        "Twenty thousand leagues under the sea, 300.0. руб. Остаток: 2 шт."
+        "Harry Potter, 100. руб. Остаток: 10 шт.\n"
+        "Lord of the Rings, 120. руб. Остаток: 9 шт.\n"
+        "The Chronicles of Narnia, 130. руб. Остаток: 12 шт.\n"
+        "Twenty thousand leagues under the sea, 300.0. руб. Остаток: 2 шт.\n"
     )
