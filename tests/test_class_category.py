7cd7341577baf92780b4352fd3bded852cf9ab66
Максим Бацманов <markgusev995@gmail.com>
2025-12-04 14:21:45 +0300

    update conftest,test_class_category, test_class_produc

--- tests/test_class_category.py
+++ tests/test_class_category.py
@@ -1,10 +1,15 @@
 from src.class_category import Category
+from src.class_product import Product
 
 
 def test_init_category(category_books):
-    assert category_books.name == 'books'
-    assert category_books.description == 'fantasy'
-    assert category_books.products == ['Harry Potter', 'Lord of the Rings', 'The Chronicles of Narnia']
+    assert category_books.name == "books"
+    assert category_books.description == "fantasy"
+    assert category_books.products == (
+        "Harry Potter, 100. руб. Остаток: 10 шт.\n"
+        "Lord of the Rings, 120. руб. Остаток: 9 шт.\n"
+        "The Chronicles of Narnia, 130. руб. Остаток: 12 шт.\n"
+    )
 
 
 def test_count_product(category_books):
@@ -16,3 +21,14 @@ def test_count_product(category_books):
 
     assert Category.product_count == 3
     assert Category.category_count == 1
+
+
+def test_add_product(category_books):
+    new_product = Product("Twenty thousand leagues under the sea", "fantasy", 300.00, 2)
+    category_books.add_product(new_product)
+    assert category_books.products == (
+        "Harry Potter, 100. руб. Остаток: 10 шт."
+        "Lord of the Rings, 120. руб. Остаток: 9 шт."
+        "The Chronicles of Narnia, 130. руб. Остаток: 12 шт."
+        "Twenty thousand leagues under the sea, 300.0. руб. Остаток: 2 шт."
+    )
