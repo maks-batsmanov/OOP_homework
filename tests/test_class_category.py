a9fe66ac43033cc7626902460c074fc60e941434
Максим Бацманов <markgusev995@gmail.com>
2025-12-04 21:12:58 +0300

    fixes  tests/test_class_category.py

--- tests/test_class_category.py
+++ tests/test_class_category.py
@@ -6,9 +6,9 @@ def test_init_category(category_books):
     assert category_books.name == "books"
     assert category_books.description == "fantasy"
     assert category_books.products == (
-        "Harry Potter, 100. руб. Остаток: 10 шт.\n"
-        "Lord of the Rings, 120. руб. Остаток: 9 шт.\n"
-        "The Chronicles of Narnia, 130. руб. Остаток: 12 шт.\n"
+        "Harry Potter, 100 руб. Остаток: 10 шт.\n"
+        "Lord of the Rings, 120 руб. Остаток: 9 шт.\n"
+        "The Chronicles of Narnia, 130 руб. Остаток: 12 шт."
     )
 
 
@@ -24,11 +24,11 @@ def test_count_product(category_books):
 
 
 def test_add_product(category_books):
-    new_product = Product("Twenty thousand leagues under the sea", "fantasy", 300.00, 2)
+    new_product = Product("Twenty thousand leagues under the sea", "fantasy", 300, 2)
     category_books.add_product(new_product)
     assert category_books.products == (
-        "Harry Potter, 100. руб. Остаток: 10 шт.\n"
-        "Lord of the Rings, 120. руб. Остаток: 9 шт.\n"
-        "The Chronicles of Narnia, 130. руб. Остаток: 12 шт.\n"
-        "Twenty thousand leagues under the sea, 300.0. руб. Остаток: 2 шт.\n"
+        "Harry Potter, 100 руб. Остаток: 10 шт.\n"
+        "Lord of the Rings, 120 руб. Остаток: 9 шт.\n"
+        "The Chronicles of Narnia, 130 руб. Остаток: 12 шт.\n"
+        "Twenty thousand leagues under the sea, 300 руб. Остаток: 2 шт."
     )
