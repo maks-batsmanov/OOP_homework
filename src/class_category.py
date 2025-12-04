eb7d1af0e3d6ef8d1d6c1f1a5b101647aa53e520
Максим Бацманов <markgusev995@gmail.com>
2025-12-04 14:08:17 +0300

    update modules class category, class product

--- src/class_category.py
+++ src/class_category.py
@@ -11,6 +11,17 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.category_count += 1
         Category.product_count += len(products)
+
+    def add_product(self, product):
+        self.__products.append(product)
+        self.product_count += 1
+
+    @property
+    def products(self):
+        result = ''
+        for product in self.__products:
+            result += f"{product.name}, {product.price}. руб. Остаток: {product.quantity} шт."
+        return result
--- src/class_product.py
+++ src/class_product.py
@@ -9,5 +9,40 @@ class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, params, list_for_check):
+
+        for instance in list_for_check:
+            if params['name'].lower() == instance.name.lower():
+
+                new_quantity = params['quantity'] + instance.quantity
+                new_price = max(params['price'], instance.__price)
+
+                instance.quantity = new_quantity
+                instance.price = new_price
+                return instance
+
+        return cls(name=params['name'],
+                   description=params['description'],
+                   price=params['price'],
+                   quantity=params['quantity'])
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, price):
+        if price <= 0:
+            print('Цена не должна быть нулевая или отрицательная')
+        elif price < self.__price:
+            print('Вы уверены, что хотите снизить цену товара?')
+            print('y - да, n - нет')
+            user_input = input('Ввод: ')
+            if user_input.lower().strip() == 'y':
+                self.__price = price
+        else:
+            self.__price = price
