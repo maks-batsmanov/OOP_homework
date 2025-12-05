9b9bde49d690589f6c149f3a1c5408a664f588e9
Максим Бацманов <markgusev995@gmail.com>
2025-12-06 00:32:44 +0300

    add tests modules smartphone and lawngrass

---
+++ tests/test_class_lawngrass.py
@@ -0,0 +1,13 @@
+from src.class_lawngrass import LawnGrass
+
+
+def test_init_lawngrass():
+    grass1 = LawnGrass("Газонная трава", "Элитная трава для газона", 500.0, 20, "Россия", "7 дней", "Зеленый")
+
+    assert grass1.name == "Газонная трава"
+    assert grass1.description == "Элитная трава для газона"
+    assert grass1.price == 500.0
+    assert grass1.quantity == 20
+    assert grass1.country == "Россия"
+    assert grass1.germination_period == "7 дней"
+    assert grass1.color == "Зеленый"
