5e165891559a99010922342e3b7d4fe2b18afd15
Максим Бацманов <markgusev995@gmail.com>
2025-12-06 21:37:05 +0300

    update main_16_1

--- src/main_16_1.py
+++ src/main_16_1.py
@@ -4,7 +4,6 @@ from src.class_smartphone import Smartphone
 from src.class_lawngrass import LawnGrass
 
 
-
 if __name__ == '__main__':
     smartphone1 = Smartphone("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, 95.5,
                          "S23 Ultra", 256, "Серый")
