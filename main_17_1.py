702a218790eb1e74ee1b4fccba015e618197d4cf
Максим Бацманов <markgusev995@gmail.com>
2025-12-14 23:54:43 +0300

    update main 17 1

--- main_17_1.py
+++ main_17_1.py
@@ -6,11 +6,7 @@ if __name__ == '__main__':
         product_invalid = Product("Бракованный товар", "Неверное количество", 1000.0, 0)
     except ValueError as e:
         print(
-            """
-            Возникла ошибка ValueError
-            прерывающая работу программы при попытке добавить продукт с нулевым количеством
-            """
-        )
+            "Возникла ошибка ValueError прерывающая работу программы при попытке добавить продукт с нулевым количеством")
     else:
         print("Не возникла ошибка ValueError при попытке добавить продукт с нулевым количеством")
 
@@ -20,7 +16,7 @@ if __name__ == '__main__':
 
     category1 = Category("Смартфоны", "Категория смартфонов", [product1, product2, product3])
 
-    print(category1.average_price())
+    print(category1.middle_price())
 
     category_empty = Category("Пустая категория", "Категория без продуктов", [])
-    print(category_empty.average_price())
+    print(category_empty.middle_price())
